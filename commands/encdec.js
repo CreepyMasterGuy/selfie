612df35179ad25fd6b897f1a79d5cfc5748d1187
Motion <motiond3v@users.noreply.github.com>
2017-01-15 21:29:05 -0600

    Update encdec

--- commands/encdec.js
+++ commands/encdec.js
@@ -1,10 +1,9 @@
-
 var Discord = require("discord.js");
 
 module.exports = {
   enc: function (toEnc) {
     m.edit(Buffer.from(toEnc).toString('base64'))
-	},
+    },
   dec: function(toDec) {
     m.edit( new Buffer(toDec, 'base64').toString())
   }
